94917219bd9b23f61e31b6542ae7877de93546bd
Pedro Rojas <pedro.rojaslo50@gmail.com>
2022-02-13 16:34:12 -0700

    Add example with arrays and dictionaries

--- BasicSyntax/BasicSyntax/main.m
+++ BasicSyntax/BasicSyntax/main.m
@@ -9,8 +9,52 @@
 
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
-        // insert code here...
-        NSLog(@"Hello, World!");
+//        NSNumber *number = [[NSNumber alloc] initWithInt:1];
+//        NSArray *array = @[number, @2, @3];
+//
+//        NSLog(@"%@", array);
+        
+//        NSDictionary *dictionary = @{
+//            @1: @"Hello",
+//            @2: @"World",
+//            @3: @"My friends"
+//        };
+        
+//        NSDictionary *dictionary = [NSDictionary
+//        dictionaryWithObjectsAndKeys:
+//                @"Hello", @1,
+//                @"World", @2,
+//                @"My friends",@3,
+//                nil
+//        ];
+        
+//        NSMutableDictionary *dictionary = [
+//            @{
+//                @1: @"Hello",
+//                @2: @"World",
+//                @3: @"My friends"
+//            }
+//
+//            mutableCopy
+//        ];
+        
+        NSMutableDictionary *dictionary = [[NSMutableDictionary alloc] initWithDictionary:@{
+                @1: @"Hello",
+                @2: @"World",
+                @3: @"My friends"
+            }
+        ];
+        
+        dictionary[@1] = @"1";
+        
+        for (NSNumber *key in dictionary) {
+            NSLog(@"%@", [dictionary objectForKey:key]);
+        }
+        
+        [dictionary enumerateKeysAndObjectsUsingBlock:^(id  _Nonnull key, id  _Nonnull obj, BOOL * _Nonnull stop) {
+            NSLog(@"%@", dictionary[key]);
+        }];
+        
     }
     return 0;
 }
