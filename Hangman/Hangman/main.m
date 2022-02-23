c292866a037dc2c49b19c5d938973833ce54ef0f
Pedro Rojas <pedro.rojaslo50@gmail.com>
2022-02-23 15:34:57 -0700

    Add Hangman project to work with C and Objective C

---
+++ Hangman/Hangman/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Hangman
+//
+//  Created by Pedro Rojas on 23/02/22.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
