6559a780212a95507b879ce40ef4fa90e4c9f1e9
Pedro Rojas <pedro.rojaslo50@gmail.com>
2022-02-28 13:04:59 -0700

    Add classes demo

---
+++ Objective-C_Classes/Objective-C_Classes/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Objective-C_Classes
+//
+//  Created by Pedro Rojas on 28/02/22.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
